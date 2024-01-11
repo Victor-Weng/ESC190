a8f757a85fa6456e0507778484b3d62b265cbf41
Victor-Weng <victorwengvtw@gmail.com>
2024-01-11 12:02:31 -0500

    First C Code and Class 2 Notes

---
+++ Class2.c
@@ -0,0 +1,12 @@
+#include <stdio.h> // import function
+
+int main() // def main() in Python (specifies that an int is returned)
+{
+    int a = 42;
+    char cl = '@';
+    char *s = "ahgkjhg";
+    // printf() is similar to python print() but DOUBLE QUOTES "" are needed.
+    printf("Hello, World!"); // semicolons are mandatory after every line
+    return 0; // Not needed but can return 0. Main function returns a value that is returned but not necessary
+}
+
