d566819716e1ccb9f75abf5461197e37fea87701
Victor-Weng <victorwengvtw@gmail.com>
2024-01-25 11:49:11 -0500

    class 6 and 7

--- Class 6.c
+++ Class 6.c
@@ -1,62 +1,69 @@
 // Including Header Files
-#include <stdio.h> //I/O functions
+#include <stdio.h>  //I/O functions
 #include <stdlib.h> //Utility functions (Memory Allocation, Type Conversions, Algorithms)
-#include <math.h> //Math functions
+#include <math.h>   //Math functions
 
-
-int function_name(){
-   return 1;
+int function_name()
+{
+    return 1;
 }
 
-struct point{
-   int x;
-   int y;
+struct point
+{
+    int x;
+    int y;
 };
 
+int main()
+{
+    printf("This is the \'Format String\'.\n"); // Print Function
+    // Declaring variables: Named memory spaces.
+    int a = 3;
+    char b = 'a';
+    double c = 3.14;
+    printf("char: %d, int: %c, double: %f\n", a, b, c); // Print Variables
+    // Declaring arrays: Named continuous set of memory spaces.
+    int arr[10]; // Array with size 10
+    arr[3] = 1;  // Accessing element of array
 
-int main() {
-   printf("This is the \'Format String\'.\n");//Print Function
-   //Declaring variables: Named memory spaces.
-   int a=3;
-   char b='a';
-   double c= 3.14;
-   printf("char: %d, int: %c, double: %f\n", a, b, c);//Print Variables
-   // Declaring arrays: Named continuous set of memory spaces.
-   int arr[10]; //Array with size 10
-   arr[3]=1; //Accessing element of array
+    // Pointer: variable that points to a memory location of a certain type
+    int i = 1;     // Defining variable
+    int *p_i = &i; // Pointer to address of variable (&variable)
+    *p_i = 2;      // Pointer accesses value of variable (*pointer)
 
-   // Pointer: variable that points to a memory location of a certain type
-   int i = 1;//Defining variable
-   int *p_i = &i;//Pointer to address of variable (&variable)
-   *p_i = 2; //Pointer accesses value of variable (*pointer)
+    char *string = "asdf";   // Defines unmodifiable char array
+    char string2[] = "asdf"; // Defines modifiable char array
 
-   char *string = "asdf"; //Defines unmodifiable char array
-   char string2[] = "asdf"; //Defines modifiable char array
+    // Structures
+    struct point p;
+    p.x = 1;
+    p.y = 0;
 
-// Structures
-   struct point p;
-   p.x=1;
-   p.y=0;
-
-// Memory Management
-// Variable memory types: 
-// local (created/destroyed in a specific section)
-// static (created/retained in specific section)
-// malloc (dynamic memory allocation)
-   // 
-   int *arr2 = (int *)malloc(10*sizeof(int));//Allocating 10 int sizes of memory
-   if(arr2==NULL){exit(1);} //Not enough memory
-   for (int i = 0; i < 10; ++i) {
+    // Memory Management
+    // Variable memory types:
+    // local (created/destroyed in a specific section)
+    // static (created/retained in specific section)
+    // malloc (dynamic memory allocation)
+    //
+    int *arr2 = (int *)malloc(10 * sizeof(int)); // Allocating 10 int sizes of memory
+    if (arr2 == NULL)
+    {
+        exit(1);
+    } // Not enough memory
+    for (int i = 0; i < 10; ++i)
+    {
         arr2[i] = i; // Initializing elements
     }
-   printf("Second element: %d\n", arr2[2]);
-   arr2 = (int *)realloc(arr2, 15*sizeof(int));//Changing to 15 int size with realloc
-   if(arr2==NULL){exit(1);} //Not enough memory
-   for (int i = 10; i < 15; ++i) {
+    printf("Second element: %d\n", arr2[2]);
+    arr2 = (int *)realloc(arr2, 15 * sizeof(int)); // Changing to 15 int size with realloc
+    if (arr2 == NULL)
+    {
+        exit(1);
+    } // Not enough memory
+    for (int i = 10; i < 15; ++i)
+    {
         arr2[i] = i; // Initializing elements
-   }
-   printf("Twelfth new element: %d", arr2[12]);
-   free(arr2);//Free memory
+    }
+    printf("Twelfth new element: %d", arr2[12]);
+    free(arr2); // Free memory
 }
-c_basics.c
-2 KB
--- Class 7.c
+++ Class 7.c
@@ -1,36 +1,79 @@
+#include <stdio.h>
+
 int *f() // returns pointer
 {
-  int arr[20];
-  arr[0] = 42;
-  return arr;
+    int arr[20];
+    arr[0] = 42;
+    return arr;
 }
 
 int *make_block_int(int sz)
 {
-  int *p = (int *)malloc(sz * sizeof(int));
-  if (p == NULL){ // malloc returns 0 if there is no memory / error
-    // use malloc to create something in local then use it / keep it outside of it back in global
-    // You can't change size of arrays, but you can change size of mallocs using re-alloc
+    int *p = (int *)malloc(sz * sizeof(int));
+    if (p == NULL)
+    { // malloc returns 0 if there is no memory / error
+        // use malloc to create something in local then use it / keep it outside of it back in global
+        // You can't change size of arrays, but you can change size of mallocs using re-alloc
 
-    printf("out of memory\n");
-    exit(1);
-  }
-  return p;
+        printf("out of memory\n");
+        exit(1);
+    }
+    return p;
 }
 
+// Structs
 
+typedef struct student
+{
+    char name[200];
+    int age;
+} student;
 
+// Sometimes, want to change the value of a pointer inside a function
 
+void set_to_0(int **p_p_a)
+{
+    *p_p_a = 0; // set the value at address p_p_a to 0
+                // p_p_a happens to be of type int **
+                // so *p_p_a is of type int *
+}
 
 int main()
 {
-  int *p = f();
-  p[0]; // undefined behavior
-        // might crash, because no guarantee of memory
-        // might sometimes print 42
-        // AVOID
-  int *q = make_block_int(20);
-  q[7] = 50; // works
+    int *p = f();
+    p[0]; // undefined behavior
+          // might crash, because no guarantee of memory
+          // might sometimes print 42
+          // AVOID
+    int *q = make_block_int(20);
+    q[7] = 50; // works
+
+    //===========================//===========================//===========================
+
+    // array:
+    student students[500]; // type student
+    student *students_block = (student *)malloc(sizeof(student) * 500);
+
+    // Dealing with Strings
+    char s1[] = "hi"; // same as char s1[] = {'h', 'i', '\0'};
+    char *s2 = 0;
+
+    // if "hi" is stored at addresses 1032, 1033, 1034
+    // if the address of 'h' is 1032, and s1 gets converted to 1032 when used
+
+    s2 = s1; // legal, but strings are now aliases. Make s2 equal to s1 (1032).
+    // s2 and s1 store the same value, which is the address of the 'h' from "hi"
+
+    strcpy(s2, s1); // not yet OK, since cannot copy to address s2
+                    // same as s2[0] = s1[0], s2[1] = s1[1], ….
+    s2 = (char *)malloc(sizeof(char) * (strlen(s1) + 1));
+    strcpy(s2, s1); // copy the contents of s1 into s2
+
+    //===========================//===========================//===========================
+
+    int a = 42;
+    int *p_a = &a;
+    set_to_0(&p_a); // p_a is now 0. a is not affected!
 }
 
 /*
@@ -44,4 +87,3 @@ On less modern OS (older operating systems), or on very light OSs, it might not 
 Memory leak: a situation where memory is allocated but never freed
 
 */
-  
